6e6633caa5d9ae614ba5ff96bdee0c53ca706546
bhavesh <bhavesh.daswani@toptal.com>
2022-03-27 19:00:25 +0530

    Added DI to make code more organized

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,10 +1,9 @@
+import { Injectable } from "@nestjs/common";
 import { MessageRepository } from "./messages.repository";
 
+@Injectable()
 export class MessagesService {
-  messagesRepository: MessageRepository;
-  constructor () {
-    this.messagesRepository = new MessageRepository;
-  }
+  constructor (public messagesRepository: MessageRepository) {}
 
   findOne(id: string) {
     return this.messagesRepository.findOne(id);
